ce213830505acf7d2aba06f1cfa5fa0aae40659f
Casey Wood <caseywood252@gmail.com>
2021-06-02 20:01:15 +0000

    initial commit for play.js

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require('net')
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+  conn.setEncoding('utf8');
+
+  conn.on('connect', function() {
+    console.log(`you are connected`)
+  });
+
+  conn.on('data', function(data) {
+    console.log(`recieved data from server: ${data}`)
+  });
+
+  return conn;
+};
+
+
+console.log('connecting....')
+connect();
